b58d676285d7ad2fe4a178a804f91fe3c0949bb3
Gleb Storozhik <helloa@noobusdeer.com>
2018-07-12 11:08:39 +0300

    rebuilding site Thu Jul 12 11:08:39 STD 2018

--- js/bg.js
+++ js/bg.js
@@ -79,8 +79,8 @@ function render() {
         camera.position.y += (-mouseY - camera.position.y) * 0.05;
     }
     else {
-        camera.position.x += (notRenderMesh.rotation.y * 20 - camera.position.x);
-        camera.position.y += (-notRenderMesh.rotation.x * 20 - camera.position.y);
+        camera.position.x += (notRenderMesh.rotation.y * 100 - camera.position.x) * 0.05;
+        camera.position.y += (-notRenderMesh.rotation.x * 100 - camera.position.y) * 0.05;
     }
     camera.lookAt(scene.position);
     renderer.render(scene, camera);
